93dac51fd5bd696b9a3a2a368fb3dd1dd7bac8c9
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2017-03-06 12:17:22 +0100

    [Whitespace] Fix whitespaces

--- Quadtastic/QuadExport.lua
+++ Quadtastic/QuadExport.lua
@@ -1,17 +1,17 @@
 local QuadExport = {}
 
 local function export_quad(filehandle, quadtable)
-	filehandle:write(string.format(
-		"{x = %d, y = %d, w = %d, h = %d}",
-		quadtable.x, quadtable.y, quadtable.w, quadtable.h))
+  filehandle:write(string.format(
+    "{x = %d, y = %d, w = %d, h = %d}",
+    quadtable.x, quadtable.y, quadtable.w, quadtable.h))
 end
 
 local function export_table_content(filehandle, table, indentation)
-	for k, v in pairs(table) do
-		filehandle:write(string.rep("  ", indentation))
-		if type(k) == "string" then
+  for k, v in pairs(table) do
+    filehandle:write(string.rep("  ", indentation))
+    if type(k) == "string" then
       filehandle:write(string.format("%s = ", k))
-		elseif type(k) ~= "number" then
+    elseif type(k) ~= "number" then
       error("Cannot handle table keys of type "..type(k))
     end
     if type(v) == "table" then
@@ -31,29 +31,29 @@ local function export_table_content(filehandle, table, indentation)
       error("Cannot handle table values of type "..type(v))
     end
     filehandle:write(",\n")
-	end
+  end
 end
 
 QuadExport.export = function(quads, filepath_or_filehandle)
-	assert(quads and type(quads) == "table")
+  assert(quads and type(quads) == "table")
 
   local filehandle
-	-- Errors need to be handled upstream
+  -- Errors need to be handled upstream
   if io.type(filepath_or_filehandle) == "file" then
     filehandle = filepath_or_filehandle
   elseif io.type(filepath_or_filehandle) == nil and
     type(filepath_or_filehandle) == "string"
   then
-  	filehandle, more = io.open(filepath_or_filehandle, "w")
-  	if not filehandle then error(more) end
+    filehandle, more = io.open(filepath_or_filehandle, "w")
+    if not filehandle then error(more) end
   else
     error("Cannot access filepath or filehandle")
   end
 
-	filehandle:write("return {\n")
-	export_table_content(filehandle, quads, 1)
-	filehandle:write("}\n")
-	filehandle:close()
+  filehandle:write("return {\n")
+  export_table_content(filehandle, quads, 1)
+  filehandle:write("}\n")
+  filehandle:close()
 end
 
 return QuadExport
