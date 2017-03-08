25b4856b25f47764751fa30cd22e1ea0be1920f7
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2017-03-08 13:09:11 +0100

    Allow initialization of data when creating states

--- Quadtastic/State.lua
+++ Quadtastic/State.lua
@@ -1,13 +1,13 @@
 local State = {}
 
 setmetatable(State, {
-  __call = function(_, name, transitions)
+  __call = function(_, name, transitions, data)
     local state = {}
     state.name = name
     state.transitions = transitions
     state.coroutine = nil -- the current coroutine
     -- Additional data
-    state.data = {}
+    state.data = data or {}
 
     return state
   end
