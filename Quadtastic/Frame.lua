77363eb73122d9ef14e0a03c863ed9d53e3fee77
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2017-03-08 16:59:23 +0100

    Modify Frame to accept custom border

--- Quadtastic/Frame.lua
+++ Quadtastic/Frame.lua
@@ -3,9 +3,9 @@ local renderutils = require("Quadtastic/Renderutils")
 local Layout = require("Quadtastic/Layout")
 local Frame = {}
 
-local quads = renderutils.border_quads(48, 0, 16, 16, 128, 128, 2)
+local default_quads = renderutils.border_quads(48, 0, 16, 16, 128, 128, 2)
 
-Frame.start = function(state, x, y, w, h)
+Frame.start = function(state, x, y, w, h, options)
   x = x or state.layout.next_x
   y = y or state.layout.next_y
 
@@ -14,14 +14,18 @@ Frame.start = function(state, x, y, w, h)
 
   -- Draw border
   love.graphics.setColor(255, 255, 255, 255)
-  renderutils.draw_border(state.style.stylesheet, quads, x, y, w, h, 2)
+  local quads = options and options.quads or default_quads
+  local bordersize = options and options.bordersize or 2
+  renderutils.draw_border(state.style.stylesheet, quads, x, y, w, h, bordersize)
 
-  Layout.start(state, x+2, y+2, w - 4, h - 4)
+  local margin = options and options.margin or 2
+  Layout.start(state, x+margin, y+margin, w - 2*margin, h - 2*margin)
 end
 
-Frame.finish = function(state, w, h)
-  state.layout.adv_x = w or state.layout.max_w + 4
-  state.layout.adv_y = h or state.layout.max_h + 4
+Frame.finish = function(state, w, h, options)
+  local margin = options and options.margin or 2
+  state.layout.adv_x = w or state.layout.max_w + 2*margin
+  state.layout.adv_y = h or state.layout.max_h + 2*margin
   Layout.finish(state)
 
 end
