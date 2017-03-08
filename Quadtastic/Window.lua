530f7cf4cd1ee68bc74d4472cba7deb9371f9c6e
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2017-03-08 17:02:04 +0100

    Window will draw border unless borderless is passed in options

--- Quadtastic/Window.lua
+++ Quadtastic/Window.lua
@@ -1,7 +1,12 @@
 local Layout = require("Quadtastic/Layout")
 local imgui = require("Quadtastic/imgui")
+local renderutils = require("Quadtastic/renderutils")
+local Frame = require("Quadtastic/Frame")
 
 local Window = {}
+
+local bordersize = 7
+local quads = renderutils.border_quads(80, 32, 16, 16, 128, 128, bordersize)
 
 Window.start = function(gui_state, x, y, w, h, options)
 	-- Store the window's bounds in the gui state
@@ -13,14 +18,22 @@ Window.start = function(gui_state, x, y, w, h, options)
 		imgui.cover_input(gui_state)
 	end
 
-	local margin = options and options.margin or 0
-	-- Enclose the window's content in a Layout
-	Layout.start(gui_state, x + margin, y + margin, w - 2*margin, h - 2*margin)
+   	local margin = options and options.margin or 4
+	if not (options and options.borderless) then
+		Frame.start(gui_state, x, y, w, h, {margin = margin, quads = quads, bordersize = bordersize})
+	else
+    	-- Enclose the window's content in a Layout
+    	Layout.start(gui_state, x + margin, y + margin, w - 2*margin, h - 2*margin)
+	end
 end
 
 Window.finish = function(gui_state, options)
-	-- Finish the window that encloses the content
-	Layout.finish(gui_state)
+	if not (options and options.borderless) then
+		Frame.finish(gui_state, nil, nil, {margin = options and options.margin or 2})
+	else
+		-- Finish the window that encloses the content
+		Layout.finish(gui_state)
+	end
 
 	local active = options and options.active or true
 	if not active then
