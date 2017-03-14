f9efbc7a12b3e0cfb722247dbc615ddd758b8057
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2017-03-14 22:43:29 +0100

    Render nested quad lists in the Quad list

--- Quadtastic/QuadList.lua
+++ Quadtastic/QuadList.lua
@@ -3,8 +3,69 @@ local Layout = require("Quadtastic/Layout")
 local Label = require("Quadtastic/Label")
 local Scrollpane = require("Quadtastic/Scrollpane")
 local imgui = require("Quadtastic/imgui")
+local libquadtastic = require("Quadtastic/libquadtastic")
 
 local QuadList = {}
+
+local function draw_quads(gui_state, quads, active, last_hovered)
+  local clicked, hovered
+  for name,quad in pairs(quads) do
+    local background_quads
+    if active[quad] then
+      background_quads = gui_state.style.rowbackground.selected
+    elseif last_hovered == quad then
+      background_quads = gui_state.style.rowbackground.hovered
+    else
+      background_quads = gui_state.style.rowbackground.default
+    end
+
+    love.graphics.setColor(255, 255, 255)
+    -- Draw row background
+    love.graphics.draw( -- top
+      gui_state.style.stylesheet, background_quads.top,
+      gui_state.layout.next_x, gui_state.layout.next_y, 
+      0, gui_state.layout.max_w, 1)
+    love.graphics.draw( -- center
+      gui_state.style.stylesheet, background_quads.center,
+      gui_state.layout.next_x, gui_state.layout.next_y + 2, 
+      0, gui_state.layout.max_w, 18)
+    love.graphics.draw( -- bottom
+      gui_state.style.stylesheet, background_quads.bottom,
+      gui_state.layout.next_x, gui_state.layout.next_y + 18, 
+      0, gui_state.layout.max_w, 1)
+
+    if libquadtastic.is_quad(quad) then
+      Label.draw(gui_state, nil, nil, gui_state.layout.max_w, nil,
+        string.format("%d: x%d y%d  %dx%d", name, quad.x, quad.y, quad.w, quad.h))
+    else
+      Label.draw(gui_state, nil, nil, gui_state.layout.max_w, nil,
+        string.format("%d: quad group", name))
+    end
+    gui_state.layout.adv_x = gui_state.layout.max_w
+    gui_state.layout.adv_y = 20
+
+    -- Check if the mouse was clicked on this list entry
+    local x, y = gui_state.layout.next_x, gui_state.layout.next_y
+    local w, h = gui_state.layout.adv_x, gui_state.layout.adv_y
+    if imgui.was_mouse_pressed(gui_state, x, y, w, h) then
+      clicked = quad
+    end
+    hovered = imgui.is_mouse_in_rect(gui_state, x, y, w, h) and quad or hovered
+
+    Layout.next(gui_state, "|")
+    -- If we are drawing a quad list, we now need to recursively draw its
+    -- children
+    if not libquadtastic.is_quad(quad) then
+      -- Use translate to add some indentation
+      love.graphics.translate(10, 0)
+      local rec_clicked, rec_hovered = draw_quads(gui_state, quad, active, last_hovered)
+      clicked = clicked or rec_clicked
+      hovered = hovered or rec_hovered
+      love.graphics.translate(-10, 0)
+    end
+  end
+  return clicked, hovered
+end
 
 -- Draw the quads in the current state.
 -- active is a table that contains for each quad whether it is active.
@@ -17,48 +78,7 @@ QuadList.draw = function(gui_state, state, x, y, w, h, active, last_hovered)
     imgui.push_style(gui_state, "font", gui_state.style.small_font)
     do state.quad_scrollpane_state = Scrollpane.start(gui_state, nil, nil, nil, nil, state.quad_scrollpane_state)
       do Layout.start(gui_state, nil, nil, nil, nil, {noscissor = true})
-        local i = 1
-        for name,quad in pairs(state.quads) do
-          local background_quads
-          if active[quad] then
-            background_quads = gui_state.style.rowbackground.selected
-          elseif last_hovered == quad then
-            background_quads = gui_state.style.rowbackground.hovered
-          else
-            background_quads = gui_state.style.rowbackground.default
-          end
-
-          love.graphics.setColor(255, 255, 255)
-          -- Draw row background
-          love.graphics.draw( -- top
-            gui_state.style.stylesheet, background_quads.top,
-            gui_state.layout.next_x, gui_state.layout.next_y, 
-            0, gui_state.layout.max_w, 1)
-          love.graphics.draw( -- center
-            gui_state.style.stylesheet, background_quads.center,
-            gui_state.layout.next_x, gui_state.layout.next_y + 2, 
-            0, gui_state.layout.max_w, 18)
-          love.graphics.draw( -- bottom
-            gui_state.style.stylesheet, background_quads.bottom,
-            gui_state.layout.next_x, gui_state.layout.next_y + 18, 
-            0, gui_state.layout.max_w, 1)
-
-          Label.draw(gui_state, nil, nil, gui_state.layout.max_w, nil,
-            string.format("%d: x%d y%d  %dx%d", i, quad.x, quad.y, quad.w, quad.h))
-          gui_state.layout.adv_x = gui_state.layout.max_w
-          gui_state.layout.adv_y = 20
-
-          -- Check if the mouse was clicked on this list entry
-          local x, y = gui_state.layout.next_x, gui_state.layout.next_y
-          local w, h = gui_state.layout.adv_x, gui_state.layout.adv_y
-          if imgui.was_mouse_pressed(gui_state, x, y, w, h) then
-            clicked = quad
-          end
-          hovered = imgui.is_mouse_in_rect(gui_state, x, y, w, h) and quad or hovered
-
-          Layout.next(gui_state, "|")
-          i = i + 1
-        end
+        clicked, hovered = draw_quads(gui_state, state.quads, active, last_hovered)
       end Layout.finish(gui_state, "|")
       -- Restrict the viewport's position to the visible content as good as
       -- possible
