7ffecf756cd9914c2fedc9c42d3f79d3afbd1275
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2017-03-14 16:50:56 +0100

    Fix set method in tableplus

--- Quadtastic/tableplus.lua
+++ Quadtastic/tableplus.lua
@@ -17,8 +17,7 @@ end
 -- That is, _set(t, v, k1, k2, k3) is another way to write t[k1][k2][k3] = v
 -- but there is no built-in way in Lua to do this in an automated way
 function table.set(tab, value, key, ...)
-  if value == nil then return
-  elseif select("#", ...) == 0 then
+  if select("#", ...) == 0 then
     tab[key] = value
   else
     -- apply recursively
