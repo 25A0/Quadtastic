8ca70f2f43fbb7b81b565fe8e5941face382f6f9
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2017-03-15 10:36:22 +0100

    Re-order elements in main window

--- Quadtastic/Quadtastic.lua
+++ Quadtastic/Quadtastic.lua
@@ -341,162 +341,165 @@ Quadtastic.draw = function(app, state, gui_state)
   love.graphics.clear(138, 179, 189)
   do Window.start(gui_state, 0, 0, w, h, {margin = 2, active = true, borderless = true})
 
-    do Layout.start(gui_state)
-      Label.draw(gui_state, nil, nil, nil, nil, "Image:")
-      Layout.next(gui_state, "-", 2)
+    do Layout.start(gui_state, nil, nil, nil, gui_state.layout.max_h - 30) -- Image editor
+      do Layout.start(gui_state, nil, nil, gui_state.layout.max_w - 160, nil)
+        do Layout.start(gui_state)
+          state.filepath = Inputfield.draw(gui_state, nil, nil, gui_state.layout.max_w - 34, nil, state.filepath)
+          Layout.next(gui_state, "-", 2)
+    
+          local pressed, active = Button.draw(gui_state, nil, nil, nil, nil, "Load")
+          if pressed then
+            app.quadtastic.load_image_from_path(state.filepath)
+          end
+          Tooltip.draw(gui_state, "Who's a good boy??")
+        end Layout.finish(gui_state, "-")
 
-      state.filepath = Inputfield.draw(gui_state, nil, nil, gui_state.layout.max_w - 34, nil, state.filepath)
-      Layout.next(gui_state, "-", 2)
+        Layout.next(gui_state, "|", 2)
 
-      local pressed, active = Button.draw(gui_state, nil, nil, nil, nil, "Load")
-      if pressed then
-        app.quadtastic.load_image_from_path(state.filepath)
-      end
-      Tooltip.draw(gui_state, "Who's a good boy??")
-    end Layout.finish(gui_state, "-")
-
-    Layout.next(gui_state, "|", 2)
-
-    do Layout.start(gui_state)
-      Label.draw(gui_state, nil, nil, nil, nil, "Quads:")
-      Layout.next(gui_state, "-", 2)
-
-      state.quadpath = Inputfield.draw(gui_state, nil, nil, gui_state.layout.max_w - 34, nil, state.quadpath or "")
-      Layout.next(gui_state, "-", 2)
-
-      local pressed, active = Button.draw(gui_state, nil, nil, nil, nil, "Load")
-      if pressed then
-        app.quadtastic.load_quads_from_path(state.quadpath)
-      end
-      Tooltip.draw(gui_state, "Who's a good boy??")
-    end Layout.finish(gui_state, "-")
-
-    Layout.next(gui_state, "|", 2)
-
-    do Layout.start(gui_state, nil, nil, nil, gui_state.layout.max_h - 30)
-      do Frame.start(gui_state, nil, nil, gui_state.layout.max_w - 160, nil)
-        if state.image then
-          ImageEditor.draw(gui_state, state)
-        else
-          -- Put a label in the center of the frame
-          local y = gui_state.layout.max_h / 2 - gui_state.style.font:getHeight()
-          Label.draw(gui_state, nil, y, gui_state.layout.max_w, nil,
-                     "no image :(", {alignment = ":"})
-        end
-      end Frame.finish(gui_state)
+        do Frame.start(gui_state)
+          if state.image then
+            ImageEditor.draw(gui_state, state)
+          else
+            -- Put a label in the center of the frame
+            local y = gui_state.layout.max_h / 2 - gui_state.style.font:getHeight()
+            Label.draw(gui_state, nil, y, gui_state.layout.max_w, nil,
+                       "no image :(", {alignment = ":"})
+          end
+        end Frame.finish(gui_state)
+      end Layout.finish(gui_state, "|")
 
       Layout.next(gui_state, "-", 2)
 
-      do Layout.start(gui_state, nil, nil, gui_state.layout.max_w - 21)
-        -- Draw the list of quads
-        local clicked, hovered = 
-          QuadList.draw(gui_state, state, nil, nil, nil, gui_state.layout.max_h - 19,
-                        state.selection, state.hovered)
-        if clicked then
-          local new_quads = {clicked}
-          -- If shift was pressed, select all quads between the clicked one and
-          -- the last quad that was clicked
-          if gui_state.input and
-            (imgui.is_key_pressed(gui_state, "lshift") or
-             imgui.is_key_pressed(gui_state, "rshift")) and
-            state.previous_clicked
-          then
-            -- Make sure that the new quad and the last quads are child of the
-            -- same parent
-            local previous_keys = {table.find_key(state.quads, state.previous_clicked)}
-            local new_keys = {table.find_key(state.quads, clicked)}
-            -- Remove the last keys since they will likely differ
-            local previous_key = table.remove(previous_keys)
-            local new_key = table.remove(new_keys)
-            if table.shallow_equals(previous_keys, new_keys) then
-              if previous_key == new_key then
-                assert(state.previous_clicked == clicked)
-                -- In this case the user clicked the same quad twice after
-                -- pressing shift. We don't need to take any extra steps.
-              else
-                -- We don't know the exact order in which quads appear. So we
-                -- iterate through the quads of the shared parent. Once we
-                -- encounter either the new or the previous quad, we start
-                -- adding all intermediate quads to a list that will then be
-                -- selected.
-                local parent = table.get(state.quads, unpack(new_keys))
-                local found_previous = false
-                local found_new = false
-                -- Clear the list of new quads to make the accumulation process
-                -- a bit easier
-                new_quads = {}
-                for k,v in pairs(parent) do
-                  if v == clicked then
-                    found_new = true
+      -- Quad list and buttons
+      do Layout.start(gui_state)
+        -- Quad file
+        do Layout.start(gui_state)
+          state.quadpath = Inputfield.draw(gui_state, nil, nil, gui_state.layout.max_w - 34, nil, state.quadpath or "")
+          Layout.next(gui_state, "-", 2)
+    
+          local pressed, active = Button.draw(gui_state, nil, nil, nil, nil, "Load")
+          if pressed then
+            app.quadtastic.load_quads_from_path(state.quadpath)
+          end
+          Tooltip.draw(gui_state, "Who's a good boy??")
+        end Layout.finish(gui_state, "-")
+
+        Layout.next(gui_state, "|", 2)
+
+        -- Quad list
+        do Layout.start(gui_state)
+          do Layout.start(gui_state, nil, nil, gui_state.layout.max_w - 21)
+            -- Draw the list of quads
+            local clicked, hovered = 
+              QuadList.draw(gui_state, state, nil, nil, nil, gui_state.layout.max_h - 19,
+                            state.selection, state.hovered)
+            if clicked then
+              local new_quads = {clicked}
+              -- If shift was pressed, select all quads between the clicked one and
+              -- the last quad that was clicked
+              if gui_state.input and
+                (imgui.is_key_pressed(gui_state, "lshift") or
+                 imgui.is_key_pressed(gui_state, "rshift")) and
+                state.previous_clicked
+              then
+                -- Make sure that the new quad and the last quads are child of the
+                -- same parent
+                local previous_keys = {table.find_key(state.quads, state.previous_clicked)}
+                local new_keys = {table.find_key(state.quads, clicked)}
+                -- Remove the last keys since they will likely differ
+                local previous_key = table.remove(previous_keys)
+                local new_key = table.remove(new_keys)
+                if table.shallow_equals(previous_keys, new_keys) then
+                  if previous_key == new_key then
+                    assert(state.previous_clicked == clicked)
+                    -- In this case the user clicked the same quad twice after
+                    -- pressing shift. We don't need to take any extra steps.
+                  else
+                    -- We don't know the exact order in which quads appear. So we
+                    -- iterate through the quads of the shared parent. Once we
+                    -- encounter either the new or the previous quad, we start
+                    -- adding all intermediate quads to a list that will then be
+                    -- selected.
+                    local parent = table.get(state.quads, unpack(new_keys))
+                    local found_previous = false
+                    local found_new = false
+                    -- Clear the list of new quads to make the accumulation process
+                    -- a bit easier
+                    new_quads = {}
+                    for k,v in pairs(parent) do
+                      if v == clicked then
+                        found_new = true
+                      end
+                      if v == state.previous_clicked then
+                        found_previous = true
+                      end
+                      if found_new or found_previous then
+                        table.insert(new_quads, v)
+                      end
+                      if found_new and found_previous then break end
+                    end
                   end
-                  if v == state.previous_clicked then
-                    found_previous = true
-                  end
-                  if found_new or found_previous then
-                    table.insert(new_quads, v)
-                  end
-                  if found_new and found_previous then break end
                 end
+              else
+                state.previous_clicked = clicked
+              end
+
+              if gui_state.input and
+                (imgui.is_key_pressed(gui_state, "lctrl") or
+                 imgui.is_key_pressed(gui_state, "rctrl"))
+              then
+                if #new_quads == 1 and Quadtastic.is_selected(state, clicked) then
+                  Quadtastic.deselect(state, new_quads)
+                else
+                  Quadtastic.select(state, new_quads)
+                end
+              else
+                Quadtastic.set_selection(state, new_quads)
               end
             end
-          else
-            state.previous_clicked = clicked
-          end
+            state.hovered = hovered
 
-          if gui_state.input and
-            (imgui.is_key_pressed(gui_state, "lctrl") or
-             imgui.is_key_pressed(gui_state, "rctrl"))
-          then
-            if #new_quads == 1 and Quadtastic.is_selected(state, clicked) then
-              Quadtastic.deselect(state, new_quads)
-            else
-              Quadtastic.select(state, new_quads)
+            Layout.next(gui_state, "|")
+
+            if Button.draw(gui_state, nil, nil, gui_state.layout.max_w, nil, "EXPORT", nil, {alignment = ":"}) then
+              app.quadtastic.export()
             end
-          else
-            Quadtastic.set_selection(state, new_quads)
-          end
-        end
-        state.hovered = hovered
-
-        Layout.next(gui_state, "|")
-
-        if Button.draw(gui_state, nil, nil, gui_state.layout.max_w, nil, "EXPORT", nil, {alignment = ":"}) then
-          app.quadtastic.export()
-        end
+          end Layout.finish(gui_state, "|")
+          Layout.next(gui_state, "-", 2)
+    
+          -- Draw button column
+          do Layout.start(gui_state)
+            if Button.draw(gui_state, nil, nil, nil, nil, nil, gui_state.style.buttonicons.rename) then
+              app.quadtastic.rename(Quadtastic.get_selection(state))
+            end
+            Tooltip.draw(gui_state, "Rename")
+            Layout.next(gui_state, "|")
+            if Button.draw(gui_state, nil, nil, nil, nil, nil, gui_state.style.buttonicons.delete) then
+              app.quadtastic.remove(Quadtastic.get_selection(state))
+            end
+            Tooltip.draw(gui_state, "Delete selected quad(s)")
+            Layout.next(gui_state, "|")
+            if Button.draw(gui_state, nil, nil, nil, nil, nil, gui_state.style.buttonicons.sort) then
+              app.quadtastic.sort(Quadtastic.get_selection(state))
+            end
+            Tooltip.draw(gui_state, "Sort unnamed quads from top to bottom, left to right")
+            Layout.next(gui_state, "|")
+            if Button.draw(gui_state, nil, nil, nil, nil, nil, gui_state.style.buttonicons.group) then
+              app.quadtastic.group(Quadtastic.get_selection(state))
+            end
+            Tooltip.draw(gui_state, "Group selected quads")
+            Layout.next(gui_state, "|")
+            Button.draw(gui_state, nil, nil, nil, nil, nil, gui_state.style.buttonicons.ungroup)
+            Tooltip.draw(gui_state, "Ungroup selected quads")
+          end Layout.finish(gui_state, "|")
+        end Layout.finish(gui_state, "-")
       end Layout.finish(gui_state, "|")
 
-      Layout.next(gui_state, "-", 2)
-
-      -- Draw button column
-      do Layout.start(gui_state)
-        if Button.draw(gui_state, nil, nil, nil, nil, nil, gui_state.style.buttonicons.rename) then
-          app.quadtastic.rename(Quadtastic.get_selection(state))
-        end
-        Tooltip.draw(gui_state, "Rename")
-        Layout.next(gui_state, "|")
-        if Button.draw(gui_state, nil, nil, nil, nil, nil, gui_state.style.buttonicons.delete) then
-          app.quadtastic.remove(Quadtastic.get_selection(state))
-        end
-        Tooltip.draw(gui_state, "Delete selected quad(s)")
-        Layout.next(gui_state, "|")
-        if Button.draw(gui_state, nil, nil, nil, nil, nil, gui_state.style.buttonicons.sort) then
-          app.quadtastic.sort(Quadtastic.get_selection(state))
-        end
-        Tooltip.draw(gui_state, "Sort unnamed quads from top to bottom, left to right")
-        Layout.next(gui_state, "|")
-        if Button.draw(gui_state, nil, nil, nil, nil, nil, gui_state.style.buttonicons.group) then
-          app.quadtastic.group(Quadtastic.get_selection(state))
-        end
-        Tooltip.draw(gui_state, "Group selected quads")
-        Layout.next(gui_state, "|")
-        Button.draw(gui_state, nil, nil, nil, nil, nil, gui_state.style.buttonicons.ungroup)
-        Tooltip.draw(gui_state, "Ungroup selected quads")
-      end Layout.finish(gui_state, "|")
-    end Layout.finish(gui_state, "-")
+    end Layout.finish(gui_state, "-") -- Image editor and quad list
 
     Layout.next(gui_state, "|", 2)
 
-    do Layout.start(gui_state)
+    do Layout.start(gui_state) -- Zoom buttons
       do
         local pressed = Button.draw(gui_state, nil, nil, nil, nil, nil, 
           gui_state.style.buttonicons.plus)
@@ -514,7 +517,7 @@ Quadtastic.draw = function(app, state, gui_state)
         end
         Tooltip.draw(gui_state, "Zoom out")
       end
-    end Layout.finish(gui_state, "-")
+    end Layout.finish(gui_state, "-") -- Zoom buttons
 
   end Window.finish(gui_state, {active = true, borderless = true})
 
