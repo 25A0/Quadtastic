a4a7aeecea854bc1f77b7a471d89ecbfca9d626a
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2017-03-23 18:39:48 +0100

    Add script to scale icons to various sizes, using aseprite's CLI

---
+++ scale_icon.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Automatically create png versions of an icon at different scales
+
+aseprite=/Applications/Aseprite.app/Contents/MacOS/aseprite
+
+icon=$1
+iconname=`echo ${icon} | sed -e 's/.ase//'`
+iconset_dir=${iconname}.iconset
+mkdir -p ${iconset_dir}
+
+scales=([1]=1 2 8 16 32)
+sizes=([1]=16 32 128 256 512)
+
+for (( i = 1; i <= ${#scales[*]}; i++ )); do
+	scale=${scales[$i]}
+	size=${sizes[$i]}
+	${aseprite} -b ${icon} --scale ${scale} --save-as ${iconset_dir}/icon_${size}x${size}.png
+done
