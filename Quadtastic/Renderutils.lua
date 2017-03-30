7e47a3bb8068b18b775fc50812f97bb26c39d169
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2017-03-30 14:20:32 +0200

    Delete obsolete function in renderutils

--- Quadtastic/Renderutils.lua
+++ Quadtastic/Renderutils.lua
@@ -16,30 +16,4 @@ return {
     love.graphics.draw(sprite, quads.c, x + s, y + s, 0, w - 2*s, h - 2*s)
   end,
 
-  border_quads = function(base_x, base_y, 
-                          width, height, 
-                          sprite_w, sprite_h,
-                          corner_size)
-    return {
-      ul = love.graphics.newQuad(
-        base_x, base_y, corner_size, corner_size, sprite_w, sprite_h),
-       l = love.graphics.newQuad(
-        base_x, base_y + corner_size, corner_size, 1, sprite_w, sprite_h),
-      ll = love.graphics.newQuad(
-        base_x, base_y + height - corner_size, corner_size, corner_size, sprite_w, sprite_h),
-       b = love.graphics.newQuad(
-        base_x + corner_size, base_y + height - corner_size, 1, corner_size, sprite_w, sprite_h),
-      lr = love.graphics.newQuad(
-        base_x + width - corner_size, base_y + height - corner_size, corner_size, corner_size, sprite_w, sprite_h),
-       r = love.graphics.newQuad(
-        base_x + width - corner_size, base_y + corner_size, corner_size, 1, sprite_w, sprite_h),
-      ur = love.graphics.newQuad(
-        base_x + width - corner_size, base_y, corner_size, corner_size, sprite_w, sprite_h),
-       t = love.graphics.newQuad(
-        base_x + corner_size, base_y, 1, corner_size, sprite_w, sprite_h),
-       c = love.graphics.newQuad(
-        base_x + corner_size, base_y + corner_size, 1, 1, sprite_w, sprite_h),
-    }
-  end,
-
 }
