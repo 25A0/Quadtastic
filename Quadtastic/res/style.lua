b6454f6e67d1a63f968177e2cc3acc238c740789
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2017-04-08 23:53:30 +0200

    Convert style.lua to new format

--- Quadtastic/res/style.lua
+++ Quadtastic/res/style.lua
@@ -1,202 +1,202 @@
-return {
-  _META = {
-    image_path = "/Users/moritz/Projects/Quadtastic/Quadtastic/res/style.png",
-  },
-  background = {x = 48, y = 16, w = 8, h = 8},
-  button_border = {
-    b = {x = 3, y = 13, w = 1, h = 3},
-    bl = {x = 0, y = 13, w = 3, h = 3},
-    br = {x = 29, y = 13, w = 3, h = 3},
-    c = {x = 3, y = 3, w = 1, h = 1},
-    l = {x = 0, y = 3, w = 3, h = 1},
-    r = {x = 29, y = 3, w = 3, h = 1},
-    t = {x = 3, y = 0, w = 1, h = 3},
-    tl = {x = 0, y = 0, w = 3, h = 3},
-    tr = {x = 29, y = 0, w = 3, h = 3},
-  },
-  button_border_disabled = {
-    b = {x = 3, y = 109, w = 1, h = 3},
-    bl = {x = 0, y = 109, w = 3, h = 3},
-    br = {x = 29, y = 109, w = 3, h = 3},
-    c = {x = 3, y = 99, w = 1, h = 1},
-    l = {x = 0, y = 99, w = 3, h = 1},
-    r = {x = 29, y = 99, w = 3, h = 1},
-    t = {x = 3, y = 96, w = 1, h = 3},
-    tl = {x = 0, y = 96, w = 3, h = 3},
-    tr = {x = 29, y = 96, w = 3, h = 3},
-  },
-  buttons = {
-    delete = {x = 96, y = 64, w = 13, h = 13},
-    group = {x = 96, y = 48, w = 13, h = 13},
-    minus = {x = 69, y = 0, w = 5, h = 5},
-    plus = {x = 64, y = 0, w = 5, h = 5},
-    rename = {x = 48, y = 64, w = 13, h = 13},
-    sort = {x = 112, y = 64, w = 13, h = 13},
-    ungroup = {x = 112, y = 48, w = 13, h = 13},
-  },
-  checkbox = {
-    checked = {x = 48, y = 32, w = 11, h = 11},
-    unchecked = {x = 32, y = 32, w = 11, h = 11},
-  },
-  crosshair = {x = 64, y = 32, w = 9, h = 9},
-  filebrowser = {
-    directory = {x = 8, y = 80, w = 8, h = 11},
-    file = {x = 0, y = 80, w = 8, h = 11},
-  },
-  frame_border = {
-    b = {x = 50, y = 14, w = 1, h = 2},
-    bl = {x = 48, y = 14, w = 2, h = 2},
-    br = {x = 62, y = 14, w = 2, h = 2},
-    c = {x = 50, y = 2, w = 1, h = 1},
-    l = {x = 48, y = 2, w = 2, h = 1},
-    r = {x = 62, y = 2, w = 2, h = 1},
-    t = {x = 50, y = 0, w = 1, h = 2},
-    tl = {x = 48, y = 0, w = 2, h = 2},
-    tr = {x = 62, y = 0, w = 2, h = 2},
-  },
-  input_field_border = {
-    b = {x = 3, y = 29, w = 1, h = 3},
-    bl = {x = 0, y = 29, w = 3, h = 3},
-    br = {x = 29, y = 29, w = 3, h = 3},
-    c = {x = 3, y = 19, w = 1, h = 1},
-    l = {x = 0, y = 19, w = 3, h = 1},
-    r = {x = 29, y = 19, w = 3, h = 1},
-    t = {x = 3, y = 16, w = 1, h = 3},
-    tl = {x = 0, y = 16, w = 3, h = 3},
-    tr = {x = 29, y = 16, w = 3, h = 3},
-  },
-  menu = {
-    arrow = {x = 96, y = 41, w = 7, h = 7},
-    b = {x = 98, y = 35, w = 1, h = 2},
-    bl = {x = 96, y = 35, w = 2, h = 2},
-    br = {x = 110, y = 35, w = 2, h = 2},
-    t = {x = 98, y = 32, w = 1, h = 2},
-    tl = {x = 96, y = 32, w = 2, h = 2},
-    tr = {x = 110, y = 32, w = 2, h = 2},
-  },
-  palette = {
-    image_editor = {
-      bg_A = {x = 2, y = 112, w = 2, h = 2},
-      bg_B = {x = 2, y = 114, w = 2, h = 2},
-    },
-    scrollpane_background = {x = 2, y = 116, w = 2, h = 2},
-    shades = {
-      bright = {x = 0, y = 118, w = 2, h = 2},
-      brightest = {x = 0, y = 120, w = 2, h = 2},
-      dark = {x = 0, y = 114, w = 2, h = 2},
-      darkest = {x = 0, y = 112, w = 2, h = 2},
-      neutral = {x = 0, y = 116, w = 2, h = 2},
-      white = {x = 0, y = 122, w = 2, h = 2},
-    },
-  },
-  rowbackground = {
-    collapsed = {
-      default = {x = 17, y = 40, w = 7, h = 7},
-      hovered = {x = 10, y = 40, w = 7, h = 7},
-      pressed = {x = 3, y = 40, w = 7, h = 7},
-    },
-    default = {
-      bottom = {x = 0, y = 46, w = 1, h = 2},
-      center = {x = 0, y = 34, w = 1, h = 1},
-      top = {x = 0, y = 32, w = 1, h = 2},
-    },
-    expanded = {
-      default = {x = 17, y = 32, w = 7, h = 7},
-      hovered = {x = 10, y = 32, w = 7, h = 7},
-      pressed = {x = 3, y = 32, w = 7, h = 7},
-    },
-    hovered = {
-      bottom = {x = 1, y = 46, w = 1, h = 2},
-      center = {x = 1, y = 34, w = 1, h = 1},
-      top = {x = 1, y = 32, w = 1, h = 2},
-    },
-    selected = {
-      bottom = {x = 2, y = 46, w = 1, h = 2},
-      center = {x = 2, y = 34, w = 1, h = 1},
-      top = {x = 2, y = 32, w = 1, h = 2},
-    },
-  },
-  scrollpane = {
-    buttons = {
-      down = {
-        default = {x = 80, y = 9, w = 7, h = 7},
-        hovered = {x = 112, y = 9, w = 7, h = 7},
-        pressed = {x = 112, y = 25, w = 7, h = 7},
-      },
-      left = {
-        default = {x = 96, y = 9, w = 7, h = 7},
-        hovered = {x = 121, y = 9, w = 7, h = 7},
-        pressed = {x = 121, y = 25, w = 7, h = 7},
-      },
-      right = {
-        default = {x = 96, y = 0, w = 7, h = 7},
-        hovered = {x = 121, y = 0, w = 7, h = 7},
-        pressed = {x = 121, y = 16, w = 7, h = 7},
-      },
-      up = {
-        default = {x = 80, y = 0, w = 7, h = 7},
-        hovered = {x = 112, y = 0, w = 7, h = 7},
-        pressed = {x = 112, y = 16, w = 7, h = 7},
-      },
-    },
-    corner = {x = 105, y = 9, w = 7, h = 7},
-    scrollbar_h = {
-      background = {x = 105, y = 0, w = 1, h = 7},
-      center = {x = 108, y = 0, w = 1, h = 7},
-      left = {x = 106, y = 0, w = 2, h = 7},
-      right = {x = 109, y = 0, w = 2, h = 7},
-    },
-    scrollbar_v = {
-      background = {x = 105, y = 0, w = 7, h = 1},
-      bottom = {x = 105, y = 4, w = 7, h = 2},
-      center = {x = 105, y = 3, w = 7, h = 1},
-      top = {x = 105, y = 1, w = 7, h = 2},
-    },
-  },
-  toast = {
-    b = {x = 2, y = 62, w = 1, h = 2},
-    bl = {x = 0, y = 62, w = 2, h = 2},
-    br = {x = 5, y = 62, w = 2, h = 2},
-    c = {x = 2, y = 59, w = 1, h = 1},
-    l = {x = 0, y = 59, w = 2, h = 1},
-    r = {x = 5, y = 59, w = 2, h = 1},
-    t = {x = 2, y = 57, w = 1, h = 2},
-    tl = {x = 0, y = 57, w = 2, h = 2},
-    tr = {x = 5, y = 57, w = 2, h = 2},
-  },
-  tools = {
-    add = {x = 32, y = 48, w = 13, h = 13},
-    border = {x = 64, y = 48, w = 13, h = 13},
-    create = {x = 48, y = 48, w = 13, h = 13},
-    select = {x = 64, y = 64, w = 13, h = 13},
-    strip = {x = 80, y = 48, w = 13, h = 13},
-  },
-  tooltip = {
-    border = {
-      b = {x = 2, y = 51, w = 1, h = 2},
-      bl = {x = 0, y = 51, w = 2, h = 2},
-      br = {x = 3, y = 51, w = 2, h = 2},
-      c = {x = 2, y = 50, w = 1, h = 1},
-      l = {x = 0, y = 50, w = 2, h = 1},
-      r = {x = 3, y = 50, w = 2, h = 1},
-      t = {x = 2, y = 48, w = 1, h = 2},
-      tl = {x = 0, y = 48, w = 2, h = 2},
-      tr = {x = 3, y = 48, w = 2, h = 2},
-    },
-    tip = {
-      downwards = {x = 5, y = 48, w = 7, h = 3},
-      upwards = {x = 5, y = 50, w = 7, h = 3},
-    },
-  },
-  window_border = {
-    b = {x = 87, y = 41, w = 1, h = 7},
-    bl = {x = 80, y = 41, w = 7, h = 7},
-    br = {x = 89, y = 41, w = 7, h = 7},
-    c = {x = 87, y = 39, w = 1, h = 1},
-    l = {x = 80, y = 39, w = 7, h = 1},
-    r = {x = 89, y = 39, w = 7, h = 1},
-    t = {x = 87, y = 32, w = 1, h = 7},
-    tl = {x = 80, y = 32, w = 7, h = 7},
-    tr = {x = 89, y = 32, w = 7, h = 7},
-  },
-}
+local t = {}
+t["_META"] = {}
+t["_META"]["image_path"] = "/Users/moritz/Projects/Quadtastic/Quadtastic/res/style.png"
+
+t["background"] = {x = 48, y = 16, w = 8, h = 8}
+t["button_border"] = {}
+t["button_border"]["b"] = {x = 3, y = 13, w = 1, h = 3}
+t["button_border"]["bl"] = {x = 0, y = 13, w = 3, h = 3}
+t["button_border"]["br"] = {x = 29, y = 13, w = 3, h = 3}
+t["button_border"]["c"] = {x = 3, y = 3, w = 1, h = 1}
+t["button_border"]["l"] = {x = 0, y = 3, w = 3, h = 1}
+t["button_border"]["r"] = {x = 29, y = 3, w = 3, h = 1}
+t["button_border"]["t"] = {x = 3, y = 0, w = 1, h = 3}
+t["button_border"]["tl"] = {x = 0, y = 0, w = 3, h = 3}
+t["button_border"]["tr"] = {x = 29, y = 0, w = 3, h = 3}
+
+t["button_border_disabled"] = {}
+t["button_border_disabled"]["b"] = {x = 3, y = 109, w = 1, h = 3}
+t["button_border_disabled"]["bl"] = {x = 0, y = 109, w = 3, h = 3}
+t["button_border_disabled"]["br"] = {x = 29, y = 109, w = 3, h = 3}
+t["button_border_disabled"]["c"] = {x = 3, y = 99, w = 1, h = 1}
+t["button_border_disabled"]["l"] = {x = 0, y = 99, w = 3, h = 1}
+t["button_border_disabled"]["r"] = {x = 29, y = 99, w = 3, h = 1}
+t["button_border_disabled"]["t"] = {x = 3, y = 96, w = 1, h = 3}
+t["button_border_disabled"]["tl"] = {x = 0, y = 96, w = 3, h = 3}
+t["button_border_disabled"]["tr"] = {x = 29, y = 96, w = 3, h = 3}
+
+t["buttons"] = {}
+t["buttons"]["delete"] = {x = 96, y = 64, w = 13, h = 13}
+t["buttons"]["group"] = {x = 96, y = 48, w = 13, h = 13}
+t["buttons"]["minus"] = {x = 69, y = 0, w = 5, h = 5}
+t["buttons"]["plus"] = {x = 64, y = 0, w = 5, h = 5}
+t["buttons"]["rename"] = {x = 48, y = 64, w = 13, h = 13}
+t["buttons"]["sort"] = {x = 112, y = 64, w = 13, h = 13}
+t["buttons"]["ungroup"] = {x = 112, y = 48, w = 13, h = 13}
+
+t["checkbox"] = {}
+t["checkbox"]["checked"] = {x = 48, y = 32, w = 11, h = 11}
+t["checkbox"]["unchecked"] = {x = 32, y = 32, w = 11, h = 11}
+
+t["crosshair"] = {x = 64, y = 32, w = 9, h = 9}
+t["filebrowser"] = {}
+t["filebrowser"]["directory"] = {x = 8, y = 80, w = 8, h = 11}
+t["filebrowser"]["file"] = {x = 0, y = 80, w = 8, h = 11}
+
+t["frame_border"] = {}
+t["frame_border"]["b"] = {x = 50, y = 14, w = 1, h = 2}
+t["frame_border"]["bl"] = {x = 48, y = 14, w = 2, h = 2}
+t["frame_border"]["br"] = {x = 62, y = 14, w = 2, h = 2}
+t["frame_border"]["c"] = {x = 50, y = 2, w = 1, h = 1}
+t["frame_border"]["l"] = {x = 48, y = 2, w = 2, h = 1}
+t["frame_border"]["r"] = {x = 62, y = 2, w = 2, h = 1}
+t["frame_border"]["t"] = {x = 50, y = 0, w = 1, h = 2}
+t["frame_border"]["tl"] = {x = 48, y = 0, w = 2, h = 2}
+t["frame_border"]["tr"] = {x = 62, y = 0, w = 2, h = 2}
+
+t["input_field_border"] = {}
+t["input_field_border"]["b"] = {x = 3, y = 29, w = 1, h = 3}
+t["input_field_border"]["bl"] = {x = 0, y = 29, w = 3, h = 3}
+t["input_field_border"]["br"] = {x = 29, y = 29, w = 3, h = 3}
+t["input_field_border"]["c"] = {x = 3, y = 19, w = 1, h = 1}
+t["input_field_border"]["l"] = {x = 0, y = 19, w = 3, h = 1}
+t["input_field_border"]["r"] = {x = 29, y = 19, w = 3, h = 1}
+t["input_field_border"]["t"] = {x = 3, y = 16, w = 1, h = 3}
+t["input_field_border"]["tl"] = {x = 0, y = 16, w = 3, h = 3}
+t["input_field_border"]["tr"] = {x = 29, y = 16, w = 3, h = 3}
+
+t["menu"] = {}
+t["menu"]["arrow"] = {x = 96, y = 41, w = 7, h = 7}
+t["menu"]["b"] = {x = 98, y = 35, w = 1, h = 2}
+t["menu"]["bl"] = {x = 96, y = 35, w = 2, h = 2}
+t["menu"]["br"] = {x = 110, y = 35, w = 2, h = 2}
+t["menu"]["t"] = {x = 98, y = 32, w = 1, h = 2}
+t["menu"]["tl"] = {x = 96, y = 32, w = 2, h = 2}
+t["menu"]["tr"] = {x = 110, y = 32, w = 2, h = 2}
+
+t["palette"] = {}
+t["palette"]["image_editor"] = {}
+t["palette"]["image_editor"]["bg_A"] = {x = 2, y = 112, w = 2, h = 2}
+t["palette"]["image_editor"]["bg_B"] = {x = 2, y = 114, w = 2, h = 2}
+
+t["palette"]["scrollpane_background"] = {x = 2, y = 116, w = 2, h = 2}
+t["palette"]["shades"] = {}
+t["palette"]["shades"]["bright"] = {x = 0, y = 118, w = 2, h = 2}
+t["palette"]["shades"]["brightest"] = {x = 0, y = 120, w = 2, h = 2}
+t["palette"]["shades"]["dark"] = {x = 0, y = 114, w = 2, h = 2}
+t["palette"]["shades"]["darkest"] = {x = 0, y = 112, w = 2, h = 2}
+t["palette"]["shades"]["neutral"] = {x = 0, y = 116, w = 2, h = 2}
+t["palette"]["shades"]["white"] = {x = 0, y = 122, w = 2, h = 2}
+
+
+t["rowbackground"] = {}
+t["rowbackground"]["collapsed"] = {}
+t["rowbackground"]["collapsed"]["default"] = {x = 17, y = 40, w = 7, h = 7}
+t["rowbackground"]["collapsed"]["hovered"] = {x = 10, y = 40, w = 7, h = 7}
+t["rowbackground"]["collapsed"]["pressed"] = {x = 3, y = 40, w = 7, h = 7}
+
+t["rowbackground"]["default"] = {}
+t["rowbackground"]["default"]["bottom"] = {x = 0, y = 46, w = 1, h = 2}
+t["rowbackground"]["default"]["center"] = {x = 0, y = 34, w = 1, h = 1}
+t["rowbackground"]["default"]["top"] = {x = 0, y = 32, w = 1, h = 2}
+
+t["rowbackground"]["expanded"] = {}
+t["rowbackground"]["expanded"]["default"] = {x = 17, y = 32, w = 7, h = 7}
+t["rowbackground"]["expanded"]["hovered"] = {x = 10, y = 32, w = 7, h = 7}
+t["rowbackground"]["expanded"]["pressed"] = {x = 3, y = 32, w = 7, h = 7}
+
+t["rowbackground"]["hovered"] = {}
+t["rowbackground"]["hovered"]["bottom"] = {x = 1, y = 46, w = 1, h = 2}
+t["rowbackground"]["hovered"]["center"] = {x = 1, y = 34, w = 1, h = 1}
+t["rowbackground"]["hovered"]["top"] = {x = 1, y = 32, w = 1, h = 2}
+
+t["rowbackground"]["selected"] = {}
+t["rowbackground"]["selected"]["bottom"] = {x = 2, y = 46, w = 1, h = 2}
+t["rowbackground"]["selected"]["center"] = {x = 2, y = 34, w = 1, h = 1}
+t["rowbackground"]["selected"]["top"] = {x = 2, y = 32, w = 1, h = 2}
+
+
+t["scrollpane"] = {}
+t["scrollpane"]["buttons"] = {}
+t["scrollpane"]["buttons"]["down"] = {}
+t["scrollpane"]["buttons"]["down"]["default"] = {x = 80, y = 9, w = 7, h = 7}
+t["scrollpane"]["buttons"]["down"]["hovered"] = {x = 112, y = 9, w = 7, h = 7}
+t["scrollpane"]["buttons"]["down"]["pressed"] = {x = 112, y = 25, w = 7, h = 7}
+
+t["scrollpane"]["buttons"]["left"] = {}
+t["scrollpane"]["buttons"]["left"]["default"] = {x = 96, y = 9, w = 7, h = 7}
+t["scrollpane"]["buttons"]["left"]["hovered"] = {x = 121, y = 9, w = 7, h = 7}
+t["scrollpane"]["buttons"]["left"]["pressed"] = {x = 121, y = 25, w = 7, h = 7}
+
+t["scrollpane"]["buttons"]["right"] = {}
+t["scrollpane"]["buttons"]["right"]["default"] = {x = 96, y = 0, w = 7, h = 7}
+t["scrollpane"]["buttons"]["right"]["hovered"] = {x = 121, y = 0, w = 7, h = 7}
+t["scrollpane"]["buttons"]["right"]["pressed"] = {x = 121, y = 16, w = 7, h = 7}
+
+t["scrollpane"]["buttons"]["up"] = {}
+t["scrollpane"]["buttons"]["up"]["default"] = {x = 80, y = 0, w = 7, h = 7}
+t["scrollpane"]["buttons"]["up"]["hovered"] = {x = 112, y = 0, w = 7, h = 7}
+t["scrollpane"]["buttons"]["up"]["pressed"] = {x = 112, y = 16, w = 7, h = 7}
+
+
+t["scrollpane"]["corner"] = {x = 105, y = 9, w = 7, h = 7}
+t["scrollpane"]["scrollbar_h"] = {}
+t["scrollpane"]["scrollbar_h"]["background"] = {x = 105, y = 0, w = 1, h = 7}
+t["scrollpane"]["scrollbar_h"]["center"] = {x = 108, y = 0, w = 1, h = 7}
+t["scrollpane"]["scrollbar_h"]["left"] = {x = 106, y = 0, w = 2, h = 7}
+t["scrollpane"]["scrollbar_h"]["right"] = {x = 109, y = 0, w = 2, h = 7}
+
+t["scrollpane"]["scrollbar_v"] = {}
+t["scrollpane"]["scrollbar_v"]["background"] = {x = 105, y = 0, w = 7, h = 1}
+t["scrollpane"]["scrollbar_v"]["bottom"] = {x = 105, y = 4, w = 7, h = 2}
+t["scrollpane"]["scrollbar_v"]["center"] = {x = 105, y = 3, w = 7, h = 1}
+t["scrollpane"]["scrollbar_v"]["top"] = {x = 105, y = 1, w = 7, h = 2}
+
+
+t["toast"] = {}
+t["toast"]["b"] = {x = 2, y = 62, w = 1, h = 2}
+t["toast"]["bl"] = {x = 0, y = 62, w = 2, h = 2}
+t["toast"]["br"] = {x = 5, y = 62, w = 2, h = 2}
+t["toast"]["c"] = {x = 2, y = 59, w = 1, h = 1}
+t["toast"]["l"] = {x = 0, y = 59, w = 2, h = 1}
+t["toast"]["r"] = {x = 5, y = 59, w = 2, h = 1}
+t["toast"]["t"] = {x = 2, y = 57, w = 1, h = 2}
+t["toast"]["tl"] = {x = 0, y = 57, w = 2, h = 2}
+t["toast"]["tr"] = {x = 5, y = 57, w = 2, h = 2}
+
+t["tools"] = {}
+t["tools"]["add"] = {x = 32, y = 48, w = 13, h = 13}
+t["tools"]["border"] = {x = 64, y = 48, w = 13, h = 13}
+t["tools"]["create"] = {x = 48, y = 48, w = 13, h = 13}
+t["tools"]["select"] = {x = 64, y = 64, w = 13, h = 13}
+t["tools"]["strip"] = {x = 80, y = 48, w = 13, h = 13}
+
+t["tooltip"] = {}
+t["tooltip"]["border"] = {}
+t["tooltip"]["border"]["b"] = {x = 2, y = 51, w = 1, h = 2}
+t["tooltip"]["border"]["bl"] = {x = 0, y = 51, w = 2, h = 2}
+t["tooltip"]["border"]["br"] = {x = 3, y = 51, w = 2, h = 2}
+t["tooltip"]["border"]["c"] = {x = 2, y = 50, w = 1, h = 1}
+t["tooltip"]["border"]["l"] = {x = 0, y = 50, w = 2, h = 1}
+t["tooltip"]["border"]["r"] = {x = 3, y = 50, w = 2, h = 1}
+t["tooltip"]["border"]["t"] = {x = 2, y = 48, w = 1, h = 2}
+t["tooltip"]["border"]["tl"] = {x = 0, y = 48, w = 2, h = 2}
+t["tooltip"]["border"]["tr"] = {x = 3, y = 48, w = 2, h = 2}
+
+t["tooltip"]["tip"] = {}
+t["tooltip"]["tip"]["downwards"] = {x = 5, y = 48, w = 7, h = 3}
+t["tooltip"]["tip"]["upwards"] = {x = 5, y = 50, w = 7, h = 3}
+
+
+t["window_border"] = {}
+t["window_border"]["b"] = {x = 87, y = 41, w = 1, h = 7}
+t["window_border"]["bl"] = {x = 80, y = 41, w = 7, h = 7}
+t["window_border"]["br"] = {x = 89, y = 41, w = 7, h = 7}
+t["window_border"]["c"] = {x = 87, y = 39, w = 1, h = 1}
+t["window_border"]["l"] = {x = 80, y = 39, w = 7, h = 1}
+t["window_border"]["r"] = {x = 89, y = 39, w = 7, h = 1}
+t["window_border"]["t"] = {x = 87, y = 32, w = 1, h = 7}
+t["window_border"]["tl"] = {x = 80, y = 32, w = 7, h = 7}
+t["window_border"]["tr"] = {x = 89, y = 32, w = 7, h = 7}
+
+return t
