133b566900d0b38b78ffd454de451d2f3d1384fc
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2017-07-01 22:45:02 +0200

    Change separation of functions in exporters module
    The previous commit extracted all functionality for loading a module from
    a file and checking it for the mandatory fields and functions that define
    whether it is a valid exporter. However, what I actually need is a function
    that checks whether a module is an exporter without any dependencies on love
    functionality. So I redid the separation, which also lead to a cleaner
    functionality scope.

--- Quadtastic/Exporters.lua
+++ Quadtastic/Exporters.lua
@@ -17,33 +17,17 @@ function exporters.init(dirname)
   assert(success)
 end
 
--- Tries to load an exporter from the given file name. Makes sure that the
--- exporter defines the mandatory functions and fields.
--- Returns the exporter if it was loaded successfully, returns nil otherwise.
-function exporters.load(file)
-  local filename, extension = common.split_extension(file)
-  if extension == "lua" then
-    -- try to load the exporter
-    local load_success, more = pcall(love.filesystem.load, file)
-    if load_success then
-      -- try to run the loaded chunk
-      local run_success, result = pcall(more)
-      if run_success then
-        if not result.name then
-          print(string.format("Exporter in %s misses name attribute.", file))
-        elseif not result.ext then
-          print(string.format("Exporter in %s misses extension.", file))
-        elseif not result.export then
-          print(string.format("Exporter in %s misses export function.", file))
-        else
-          return result
-        end
-      else
-        print("Exporter could not be executed: " .. result)
-      end
-    else
-      print("Could not load exporter in file " .. file ..": " .. more)
-    end
+-- Checks whether the given module conforms to the requirements of an exporter.
+-- That is, the module needs to define the mandatory functions and fields.
+function exporters.is_exporter(module)
+  if not module.name then
+    return false, "Module misses name attribute."
+  elseif not module.ext then
+    return false, "Module misses extension attribute."
+  elseif not module.export then
+    return false, "Module misses export function."
+  else
+    return true
   end
 end
 
@@ -53,18 +37,47 @@ function exporters.list(dirname)
   local found_exporters = {}
   local num_found = 0
   if love.filesystem.exists(dirname) then
+
     local files = love.filesystem.getDirectoryItems(dirname)
     for _, file in ipairs(files) do
-      local result = exporters.load(dirname .. "/" .. file)
-      if result then
-        if found_exporters[result.name] then
-          print(string.format("Exporter in %s declares a name (%s) that already exists.",
-                              file, result.name))
+
+      local filename, extension = common.split_extension(file)
+      if extension == "lua" then
+
+        -- try to load the exporter
+        local load_success, more = pcall(love.filesystem.load, dirname .. "/" .. file)
+        if load_success then
+
+          -- try to run the loaded chunk
+          local run_success, result = pcall(more)
+          if run_success then
+
+            local is_exporter, reason = exporters.is_exporter(result)
+            if is_exporter then
+
+              -- Check for naming conflicts
+              if found_exporters[result.name] then
+                print(string.format("Exporter in %s declares a name (%s) that already exists.",
+                                    file, result.name))
+              else --if has no name conflict
+                found_exporters[result.name] = result
+                num_found = num_found + 1
+              end
+
+            else
+              print(string.format("Module in file %s is not an exporter: %s",
+                                  file, reason))
+            end -- if is exporter
+
+          else
+            print("Exporter could not be executed: " .. result)
+          end -- if can run
+
         else
-          found_exporters[result.name] = result
-          num_found = num_found + 1
-        end
-      end
+          print("Could not load exporter in file " .. file ..": " .. more)
+        end -- if can load
+
+      end -- if has .lua extension
     end -- for file in dir
   end -- if is dir
   return found_exporters, num_found
