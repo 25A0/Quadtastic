bb4d6cb91ab04018d1d65fb6e2ac9ae13bbb76e7
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2017-07-02 01:08:44 +0200

    Add more test cases

--- test_exporters/export_test_cases.lua
+++ test_exporters/export_test_cases.lua
@@ -6,6 +6,13 @@ return {
     {["a quad"] = {x = 4, y = 12, w = 8, h = 8}},
   },
 
+  ["Metatable"] = {
+    {
+      ["a quad"] = {x = 4, y = 12, w = 8, h = 8},
+      ["_META"] = {image_path = "foobar"},
+    },
+  },
+
   ["Nested group"] = {
     {
       ["a group"] = {
@@ -17,4 +24,40 @@ return {
       ["a quad"] = {x = 4, y = 4, w = 8, h = 8},
     },
   },
+
+  ["Mixed key types"] = {
+    {
+      ["a group"] = {
+        ["first quad"] = {x = 4, y = 12, w = 4, h = 8},
+        ["second quad"] = {x = 8, y = 12, w = 4, h = 8},
+        ["third quad"] = {x = 12, y = 12, w = 4, h = 8},
+        {x = 16, y = 12, w = 4, h = 8},
+        [4] = {x = 20, y = 12, w = 4, h = 8},
+      },
+      ["a quad"] = {x = 4, y = 4, w = 8, h = 8},
+    },
+  },
+
+  ["Names that make a group look like a quad"] = {
+    {
+      ["a group"] = {
+        x = {x = 4, y = 12, w = 4, h = 8},
+        y = {x = 8, y = 12, w = 4, h = 8},
+        w = {x = 12, y = 12, w = 4, h = 8},
+        h = {x = 16, y = 12, w = 4, h = 8},
+      },
+    },
+  },
+
+  ["Special characters"] = {
+    {
+      ["a group"] = {
+        ["first qüäd"] = {x = 4, y = 12, w = 4, h = 8},
+        ["ßecønd quad"] = {x = 8, y = 12, w = 4, h = 8},
+        ["third/\\}\";'' quad"] = {x = 12, y = 12, w = 4, h = 8},
+      },
+      ["a quad"] = {x = 4, y = 4, w = 8, h = 8},
+    },
+  },
+
 }
