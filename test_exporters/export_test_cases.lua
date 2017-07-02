04040bf51763c1d48b75f9650c3f391d0ffc30b8
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2017-07-03 01:35:20 +0200

    Add example project to test cases

--- test_exporters/export_test_cases.lua
+++ test_exporters/export_test_cases.lua
@@ -113,4 +113,25 @@ return {
     },
   },
 
+  ["The example project"] = {
+    {
+      _META = {
+        image_path = "foobar/res/sheet.png",
+      },
+      base = {x = 16, y = 27, w = 16, h = 8},
+      bubbles = {
+        {x = 2, y = 18, w = 5, h = 5},
+        {x = 1, y = 25, w = 3, h = 4},
+        {x = 10, y = 18, w = 5, h = 3},
+        {x = 7, y = 24, w = 7, h = 6},
+        {x = 3, y = 8, w = 5, h = 4},
+        {x = 10, y = 11, w = 4, h = 3},
+        {x = 7, y = 3, w = 6, h = 4},
+      },
+      lid = {x = 16, y = 7, w = 16, h = 15},
+      liquid = {x = 0, y = 32, w = 3, h = 3},
+      stand = {x = 32, y = 32, w = 16, h = 16},
+    },
+  },
+
 }
