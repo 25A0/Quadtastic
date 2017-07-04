6954faaf89c29dcb433e788fe88cf59960fe372c
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2017-07-04 07:10:06 +0200

    Simplify QuadExport.export to only accept filepaths, not file handles

--- Quadtastic/QuadExport.lua
+++ Quadtastic/QuadExport.lua
@@ -3,7 +3,7 @@ local common = require(current_folder.. ".common")
 
 local QuadExport = {}
 
-QuadExport.export = function(quads, exporter, filepath_or_filehandle)
+QuadExport.export = function(quads, exporter, filepath)
   assert(quads and type(quads) == "table")
   assert(exporter and type(exporter) == "table", tostring(type(exporter)))
   assert(exporter.export and type(exporter.export) == "function")
@@ -11,28 +11,18 @@ QuadExport.export = function(quads, exporter, filepath_or_filehandle)
   assert(exporter.name and type(exporter.name) == "string")
 
 
-  local filehandle, more
-  -- Errors need to be handled upstream
-  if io.type(filepath_or_filehandle) == "file" then
-    filehandle = filepath_or_filehandle
-  elseif io.type(filepath_or_filehandle) == nil and
-    type(filepath_or_filehandle) == "string"
-  then
-    filehandle, more = io.open(filepath_or_filehandle, "w")
-    if not filehandle then error(more) end
-  else
-    error("Cannot access filepath or filehandle")
-  end
+  local filehandle, open_err = io.open(filepath, "w")
+  if not filehandle then error(open_err) end
 
   -- Insert version info into quads
   if not quads._META then quads._META = {} end
   quads._META.version = common.get_version()
 
   local writer = common.get_writer(filehandle)
-  local success, more = pcall(exporter.export, writer, quads)
+  local success, export_err = pcall(exporter.export, writer, quads)
   filehandle:close()
 
-  if not success then error(more, 0) end
+  if not success then error(export_err, 0) end
 end
 
 return QuadExport
